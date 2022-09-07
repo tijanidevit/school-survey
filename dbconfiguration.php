89639ccacf607e8607da918f99ead3c1f16947c8
tijanidevit <tijanidevit@yahoo.com>
2022-09-07 23:24:02 +0100

    db updated

--- dbconfiguration.php
+++ dbconfiguration.php
@@ -1,10 +1,17 @@
 <?php
-
+//mysql://b0a969d21c295a:94cc4f3c@eu-cdbr-west-03.cleardb.net/heroku_2ec20082ec190ef?reconnect=true
 // Define some variables
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "student_course";
+// $hostname = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "student_course";
+
+
+
+$hostname = "eu-cdbr-west-03.cleardb.net";
+$username = "b0a969d21c295a";
+$password = "94cc4f3c";
+$database = "heroku_2ec20082ec190ef";
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
 
